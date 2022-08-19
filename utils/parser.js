89172acee7ec423c330497232dfb7dc87f66cbaf
Boo Sung Jun <boorooksus@gmail.com>
2022-08-19 14:30:42 +0900

    [GYJB-145] 미리보기 이미지 오류 수정

--- utils/parser.js
+++ utils/parser.js
@@ -1,35 +1,40 @@
-const axios = require("axios");
+const axios = require('axios');
 
 const getFullURL = (url) => {
   let res = url;
   if (!/^(https?:\/\/)/g.test(url)) {
-    res = "https://" + res;
+    res = 'https://' + res;
   }
   return res;
 };
 
 const parse = async (url) => {
-  const response = await axios.get(encodeURI(getFullURL(url)).replace(/^([^?#]*).*/, "$1")).catch((err) => {
-    console.log(err);
-  });
-
-  console.log("hji");
+  const response = await axios
+    .get(encodeURI(getFullURL(url)).replace(/^([^?#]*).*/, '$1'))
+    .catch((err) => {
+      console.log(err);
+    });
 
   if (response.status >= 400) {
     throw response;
   }
 
-  const res = { linkImg: "", title: "", text: "", url: encodeURI(getFullURL(url)) };
+  const res = {
+    linkImg: '',
+    title: '',
+    text: '',
+    url: encodeURI(getFullURL(url)),
+  };
   const html = response.data;
 
   const textArr = html.match(
-    /<(p|h1|h2|h3|h4|h5|td|b|span|a)[^>]*>[\r\n\t\s]*([^<]+)[\r\n\t\s]*<\/(p|h1|h2|h3|h4|h5|td|b|span|a)>/gim
+    /<(p|h1|h2|h3|h4|h5|td|b|span|a)[^>]*>[\r\n\t\s]*([^<]+)[\r\n\t\s]*<\/(p|h1|h2|h3|h4|h5|td|b|span|a)>/gim,
   );
   if (!!textArr && !!textArr.length) {
     res.text = textArr
-      .join(" ")
-      .replaceAll(/<[^>]*>/gim, "")
-      .replaceAll(/(  +)|(\r)|(\n)|(\t)|(\\+r)|(\\+n)|(\\+t)|/gim, "");
+      .join(' ')
+      .replaceAll(/<[^>]*>/gim, '')
+      .replaceAll(/(  +)|(\r)|(\n)|(\t)|(\\+r)|(\\+n)|(\\+t)|/gim, '');
   }
 
   const OGImgTags = html.match(/<meta[^>]+og:image[^>]+>/gim);
@@ -40,31 +45,36 @@ const parse = async (url) => {
       const imgURL = imgURLs[0].slice(0, -1);
 
       const response = await axios
-        .get(encodeURI(imgURL).replace(/^([^?#]*).*/, "$1"))
+        .get(encodeURI(imgURL).replace(/^([^?#]*).*/, '$1'))
         .then(() => {
           res.linkImg = imgURL;
         })
         .catch((err) => {
           console.log(err);
         });
-    }
-  } else {
-    if (!!imgTags && !!imgTags.length) {
-      const imgTags = html.match(/<img[^>]*\/?>/gim);
 
+      // res.linkImg = imgURL;
+    }
+  }
+
+  if (!res.linkImg) {
+    const imgTags = html.match(/<img[^>]*\/?>/gim);
+    if (!!imgTags && !!imgTags.length) {
       for (let imgTag of imgTags) {
         const imgURLs = imgTag.match(/(https?:\/\/)[^("|')]+("|')/g);
 
         if (!!imgURLs) {
           const imgURL = imgURLs[0].slice(0, -1);
           const response = await axios
-            .get(encodeURI(imgURL).replace(/^([^?#]*).*/, "$1"))
+            .get(encodeURI(imgURL).replace(/^([^?#]*).*/, '$1'))
             .then(() => {
               res.linkImg = imgURL;
             })
             .catch((err) => {
               console.log(err);
             });
+          // res.linkImg = imgURL;
+
           if (!!res.linkImg) {
             break;
           }
@@ -73,10 +83,15 @@ const parse = async (url) => {
     }
   }
 
-  const titles = html.match(/<title[^>]*>[\r\n\t\s]*([^<]+)[\r\n\t\s]*<\/title>/gim);
+  const titles = html.match(
+    /<title[^>]*>[\r\n\t\s]*([^<]+)[\r\n\t\s]*<\/title>/gim,
+  );
 
   if (!!titles) {
-    res.title = titles[0].replace(/<title[^>]*>[\r\n\t\s]*([^<]+)[\r\n\t\s]*<\/title>/gim, "$1");
+    res.title = titles[0].replace(
+      /<title[^>]*>[\r\n\t\s]*([^<]+)[\r\n\t\s]*<\/title>/gim,
+      '$1',
+    );
   }
 
   return res;
