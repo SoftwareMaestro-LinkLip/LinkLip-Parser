59be6ec597063adf0fe3227db4354647fce8bea6
Boo Sung Jun <boorooksus@gmail.com>
2022-09-22 10:05:44 +0900

    [GYJB-221] 엔드포인트를 POST 방식으로 변경

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const { parse } = require('./utils/parser');
+
+// application/x-www-form-urlencoded 데이터를 분석해서 가져올 수 있게함
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/jason 을 분석해서 가져올 수 있게함
+app.use(bodyParser.json());
 
 app.use(cors());
 
-app.get('/link/v1', (req, res) => {
+app.post('/link/v1', (req, res) => {
   let url = req.query.url;
 
-  parse(url)
-    .then((data) => {
-      return res.status(200).json({
-        success: true,
-        data,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(401).json({
-        success: false,
-        err,
-      });
+  parse(req.body.url).then((data) => {
+    return res.status(200).json({
+      success: true,
+      data,
     });
+  });
 });
 
 const port = process.env.PORT || 5050;
